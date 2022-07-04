86016f8ae0f0391205b604a55e83e616e23c6bde
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-07-04 18:43:42 -0400

    Added list of usernames

--- Assignments/assignment01/routes.js
+++ Assignments/assignment01/routes.js
@@ -1,3 +1,13 @@
+const fs = require("fs");
+
+const read = (path, type) =>
+    new Promise((resolve, reject) => {
+        fs.readFile(path, type, (err, file) => {
+            if (err) reject(err);
+            resolve(file);
+        });
+    });
+
 const routeHandler = (req, res) => {
     const { url, method } = req;
     const body = [];
@@ -26,7 +36,9 @@ const routeHandler = (req, res) => {
         req.on("end", () => {
             const parsedData = Buffer.concat(body).toString();
             const usernameInput = parsedData.split("=")[1];
-            console.log(usernameInput);
+            fs.appendFile("users.txt", `${usernameInput}\r\n`, (err) => {
+                console.log(err);
+            });
             res.setHeader("Content-Type", "text/html");
             res.writeHead(302, { Location: "/users" });
             return res.end();
@@ -34,18 +46,24 @@ const routeHandler = (req, res) => {
     }
 
     if (url === "/users") {
-        console.log(req.body);
         res.setHeader("Content-Type", "text/html");
         res.write("<html>");
         res.write("<head><title>User Page</title></head>");
         res.write("<body>");
-        res.write(
-            `<h2>Username ${body[0]} has been successfully created!</h2>`
-        );
+        res.write(`<h2>Username has been successfully created!</h2>`);
         res.write("<ul>");
-        for (let username of body) {
-            res.write(`<li>${username}</li>`);
-        }
+
+        read("users.txt", "utf8")
+            .then((fileData) =>
+                fileData
+                    .split(/\r\n/)
+                    .forEach((username) =>
+                        username.replace(/\r\n/g, " ") === " "
+                            ? console.log("end")
+                            : res.write(`<li>${username}</li>`)
+                    )
+            )
+            .catch((err) => console.log("error reading file " + err));
         res.write("</ul>");
         res.write("</body>");
         res.write("</html>");
