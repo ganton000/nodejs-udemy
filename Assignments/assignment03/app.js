047f26b5071ec43bfef695cc37c870439f9fc1bf
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-07-05 19:06:41 -0400

    Set up Pug engine and boilerplate

--- Assignments/assignment03/app.js
+++ Assignments/assignment03/app.js
@@ -1,8 +1,13 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 
 const app = express();
 const PORT = 3001;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set("view engine", "pug");
+app.set("views", "views");
 
 const mainRouter = require("./routes/main");
 const addUserRouter = require("./routes/addUser");
@@ -11,7 +16,7 @@ app.use(addUserRouter);
 app.use(mainRouter);
 
 app.use((req, res, next) => {
-    console.log("404");
+    res.status(404).render("404");
 });
 
 app.listen(PORT, () => {
