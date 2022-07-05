43f57add61bee8e56614928568a9daa123d7c2c8
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-07-05 14:14:32 -0400

    initial commit

---
+++ server-express-demo-2/app.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+const PORT = 3001;
+
+//parses req.body sent through forms
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//to serve static files: pass in folder to grant read-access to
+app.use(express.static(path.join(__dirname, "public")));
+
+//set up routes
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+//catch all to return page not found for unmatched paths
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+
+app.listen(PORT);
