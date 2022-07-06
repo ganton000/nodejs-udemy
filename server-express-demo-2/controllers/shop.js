0de80d112b86290b18092ae985f8fd6c08a5b07a
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-07-06 13:49:45 -0400

    Added Orders route/controller/nav-link

--- server-express-demo-2/controllers/shop.js
+++ server-express-demo-2/controllers/shop.js
@@ -28,3 +28,7 @@ exports.getCart = (req, res, next) => {
 exports.getCheckout = (req, res, next) => {
     res.render("shop/checkout", { path: "/checkout", docTitle: "Checkout" });
 };
+
+exports.getOrders = (req, res, next) => {
+    res.render("shop/orders", { path: "/orders", docTitle: "Your Orders" });
+};
