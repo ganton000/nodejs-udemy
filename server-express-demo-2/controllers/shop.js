06f992b69b9696c2bebf40ba99ed67d5eac45a2a
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-07-07 15:38:51 -0400

    Add delete cart item functionality in cart.ejs

--- server-express-demo-2/controllers/shop.js
+++ server-express-demo-2/controllers/shop.js
@@ -66,6 +66,15 @@ exports.postCart = (req, res, next) => {
     });
     res.redirect("/cart");
 };
+
+exports.postCartDeleteProduct = (req, res, next) => {
+    const { productId } = req.body;
+    Product.findById(productId, (product) => {
+        Cart.deleteProduct(productId, product.price);
+    });
+    res.redirect("/cart");
+};
+
 exports.getCheckout = (req, res, next) => {
     res.render("shop/checkout", { path: "/checkout", docTitle: "Checkout" });
 };
