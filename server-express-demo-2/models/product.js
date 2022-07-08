d86f64c7d54527d26e414993730ccf4130e8997b
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-07-08 19:43:03 -0400

    Refactor product.js using sequelize

--- server-express-demo-2/models/product.js
+++ server-express-demo-2/models/product.js
@@ -1,44 +1,30 @@
-//const fs = require("fs");
-//const path = require("path");
-const db = require("../utils/database");
-const Cart = require("./cart");
+const Sequelize = require("sequelize");
+const sequelize = require("../utils/database");
 
-//const rootDir = path.dirname(require.main.filename);
-//const p = path.join(rootDir, "data", "products.json");
+//defines models
+const Product = sequelize.define("product", {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true,
+    },
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    price: {
+        type: Sequelize.DOUBLE,
+        allowNull: false,
+    },
+    imageUrl: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+});
 
-//const getProductsFromFile = (cb) => {
-//    fs.readFile(p, (err, fileContent) => {
-//        if (err) {
-//            return cb([]);
-//        } else {
-//            return cb(JSON.parse(fileContent));
-//        }
-//    });
-//};
-
-module.exports = class Product {
-    constructor(id, title, imageUrl, description, price) {
-        this.id = id;
-        this.title = title;
-        this.imageUrl = imageUrl;
-        this.description = description;
-        this.price = price;
-    }
-
-    save() {
-        return db.execute(
-            "INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)",
-            [this.title, this.price, this.imageUrl, this.description]
-        );
-    }
-
-    static deleteById(id) {}
-    //call on class
-    static fetchAll() {
-        return db.execute("SELECT * FROM products");
-    }
-
-    static findById(id) {
-        return db.execute("SELECT * FROM products WHERE products.id = ?", [id]);
-    }
-};
+module.exports = Product;
