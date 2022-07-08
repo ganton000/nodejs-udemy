70030e5ed80f2e2d0316267cb3943e5982c7e9e4
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-07-08 19:38:15 -0400

    Connect Sequelize to databae instead of mysql2

--- server-express-demo-2/utils/database.js
+++ server-express-demo-2/utils/database.js
@@ -1,12 +1,20 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 require("dotenv").config();
 
-//manages multiple connections to maintain multiple queries
-const pool = mysql.createPool({
-    host: process.env.HOST,
-    user: process.env.USERNAME,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-});
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.USERNAME,
+    process.env.PASSWORD,
+    { dialect: "mysql", host: process.env.HOST }
+);
 
-module.exports = pool.promise();
+module.exports = sequelize;
+
+////manages multiple connections to maintain multiple queries
+//const pool = mysql.createPool({
+//    host: process.env.HOST,
+//    user: process.env.USERNAME,
+//    database: process.env.DB_NAME,
+//    password: process.env.DB_PASSWORD,
+//});
+//module.exports = pool.promise();
