bebfe234424b2eb4704111fd347c5bceb78ac5b9
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-07-09 14:40:29 -0400

    Refactored admin.js controller to use sequelize orm for db connection

--- server-express-demo-2/controllers/admin.js
+++ server-express-demo-2/controllers/admin.js
@@ -22,8 +22,11 @@ exports.postAddProduct = (req, res, next) => {
         price,
         imageUrl,
     })
-        .then(res => console.log('Product Created'))
-        .catch(err => {
+        .then((res) => {
+            console.log("Product Created");
+            res.redirect("/admin/products");
+        })
+        .catch((err) => {
             console.log(err);
         });
 };
@@ -36,23 +39,25 @@ exports.getEditProduct = (req, res, next) => {
         return res.redirect("/");
     }
 
-    Product.findById(productId, (product) => {
-        if (!product) {
-            return res.redirect("/");
-        }
-        res.render("admin/edit-product", {
-            docTitle: "Edit Product",
-            path: "/admin/edit-product",
-            editing: edit,
-            product: product,
-            buttonAction: edit ? "Update" : "Delete",
-            action: edit ? "edit-product" : "delete-product",
-            title: edit ? product.title : "",
-            imageUrl: edit ? product.imageUrl : "",
-            price: edit ? product.price : "",
-            description: edit ? product.description : "",
-        });
-    });
+    Product.findByPk(productId)
+        .then((product) => {
+            if (!product) {
+                return res.redirect("/");
+            }
+            res.render("admin/edit-product", {
+                docTitle: "Edit Product",
+                path: "/admin/edit-product",
+                editing: edit,
+                product: product,
+                buttonAction: edit ? "Update" : "Delete",
+                action: edit ? "edit-product" : "delete-product",
+                title: edit ? product.title : "",
+                imageUrl: edit ? product.imageUrl : "",
+                price: edit ? product.price : "",
+                description: edit ? product.description : "",
+            });
+        })
+        .catch((err) => console.log(err));
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -63,29 +68,42 @@ exports.postEditProduct = (req, res, next) => {
         imageUrl: updatedImageUrl,
         description: updatedDesc,
     } = req.body;
-    const updatedProduct = new Product(
-        productId,
-        updatedTitle,
-        updatedImageUrl,
-        updatedDesc,
-        updatedPrice
-    );
-    updatedProduct.save();
-    res.redirect("/admin/products");
+    Product.findByPk(productId)
+        .then((product) => {
+            product.title = updatedTitle
+            product.price = updatedPrice
+            product.description = updatedDesc
+            product.imageUrl = updatedImageUrl
+            return product.save();
+        })
+        .then((result) => {
+            console.log("Updated Product");
+            res.redirect("/admin/products");
+        })
+        .catch((err) => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll((products) => {
-        res.render("admin/products", {
-            products,
-            path: "/admin/products",
-            docTitle: "My Products",
-        });
-    });
+    Product.findAll()
+        .then((products) => {
+            res.render("admin/products", {
+                products,
+                path: "/admin/products",
+                docTitle: "My Products",
+            });
+        })
+        .catch((err) => console.log(err));
 };
 
 exports.postDeleteProduct = (req, res, next) => {
     const { productId } = req.body;
-    Product.deleteById(productId);
-    res.redirect("/admin/products");
+    Product.findByPk(productId)
+        .then((product) => {
+            return product.destroy();
+        })
+        .then((result) => {
+            console.log("Product Removed");
+            res.redirect("/admin/products");
+        })
+        .catch((err) => console.log(err));
 };
