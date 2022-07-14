efc3336553fcf573a548eb0307f7a9829591de32
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-07-14 13:56:20 -0400

    Created Order/Order-Item Model

--- server-express-demo-2/app.js
+++ server-express-demo-2/app.js
@@ -10,6 +10,8 @@ const Product = require("./models/product");
 const User = require("./models/user");
 const Cart = require("./models/cart");
 const CartItem = require("./models/cart-item");
+const Order = require("./models/order.js");
+const OrderItem = require("./models/order-item");
 
 const app = express();
 
@@ -66,6 +68,10 @@ User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
+Product.belongsToMany(Order, { through: OrderItem });
 
 sequelize
     //.sync({ force: true })
--- server-express-demo-2/controllers/shop.js
+++ server-express-demo-2/controllers/shop.js
@@ -133,10 +133,19 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
     const { productId } = req.body;
-    Product.findById(productId, (product) => {
-        Cart.deleteProduct(productId, product.price);
-    });
-    res.redirect("/cart");
+    req.user
+        .getCart()
+        .then((cart) => {
+            return cart.getProducts({ where: { id: productId } });
+        })
+        .then((products) => {
+            const product = products[0];
+            return product.cartItem.destroy();
+        })
+        .then((result) => {
+            res.redirect("/cart");
+        })
+        .catch((err) => console.log(err));
 };
 
 exports.getCheckout = (req, res, next) => {
