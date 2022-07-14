4b96de07c778ebe6e8558803d11f34acd0065cad
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-07-14 11:32:39 -0400

    sequelied postAddCartProduct

--- server-express-demo-2/controllers/shop.js
+++ server-express-demo-2/controllers/shop.js
@@ -98,10 +98,37 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
     const { productId } = req.body;
-    Product.findById(productId, (product) => {
-        Cart.addProduct(productId, product.price);
-    });
-    res.redirect("/cart");
+    let fetchedCart;
+    let newQuantity = 1;
+    req.user
+        .getCart()
+        .then((cart) => {
+            fetchedCart = cart;
+            return cart.getProducts({ where: { id: productId } });
+        })
+        .then((products) => {
+            let product;
+            if (products.length > 0) {
+                product = products[0];
+            }
+            if (product) {
+                const oldQuantity = product.cartItem.quantity;
+                newQuantity = oldQuantity + 1;
+                return product;
+            }
+            return Product.findByPk(productId);
+        })
+        .then((product) => {
+            return fetchedCart.addProduct(product, {
+                through: {
+                    quantity: newQuantity,
+                },
+            });
+        })
+        .then(() => {
+            res.redirect("/cart");
+        })
+        .catch((err) => console.log(err));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
