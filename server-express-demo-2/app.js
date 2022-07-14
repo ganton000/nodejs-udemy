39a9b9ce7a0ed69f64b91ce0f772ddabe39008bf
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-07-14 10:31:21 -0400

    Added Cart and Cart-Item Model, created db tables with associated keys for them

--- server-express-demo-2/app.js
+++ server-express-demo-2/app.js
@@ -8,6 +8,8 @@ const errorController = require("./controllers/error");
 const sequelize = require("./utils/database");
 const Product = require("./models/product");
 const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
 
 const app = express();
 
@@ -60,8 +62,13 @@ app.use(errorController.get404Page);
 //create relationships between models
 Product.belongsTo(User, { constraints: true, onelete: "CASCADE" });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
+    //.sync({ force: true })
     .sync()
     .then((result) => {
         return User.findByPk(1);
