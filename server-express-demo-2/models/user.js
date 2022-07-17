a4761f22fd291c41131d43dac27e8bd95119aa52
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-07-17 14:52:09 -0400

    Fixed order.ejs to work with mongodb item, updated postOrders and getOrders in shop controller

--- server-express-demo-2/models/user.js
+++ server-express-demo-2/models/user.js
@@ -84,6 +84,41 @@ class User {
         );
     }
 
+    addOrder() {
+        const db = getDb();
+
+        return this.getCart()
+            .then((products) => {
+                const order = {
+                    items: products,
+                    user: {
+                        _id: mongodb.ObjectId(this._id),
+                        name: this.name,
+                        email: this.email,
+                    },
+                };
+                return db.collection("orders").insertOne(order);
+            })
+            .then((result) => {
+                this.cart = { items: [] };
+                return db.collection("users").updateOne(
+                    {
+                        _id: mongodb.ObjectId(this._id),
+                    },
+                    { $set: { cart: { items: [] } } }
+                );
+            })
+            .catch((err) => console.log(err));
+    }
+
+    getOrders() {
+        const db = getDb();
+        return db
+            .collection("orders")
+            .find({ "user._id": mongodb.ObjectId(this._id) })
+            .toArray();
+    }
+
     static findById(userId) {
         const db = getDb();
         return db
