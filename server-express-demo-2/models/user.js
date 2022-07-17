5d0cbdf97ee5e13366c5f675e229e05691d916d1
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-07-17 17:46:01 -0400

    set up product/user schema boilerplate

--- server-express-demo-2/models/user.js
+++ server-express-demo-2/models/user.js
@@ -1,3 +1,18 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+});
+
 //const mongodb = require("mongodb");
 //const { getProducts } = require("../controllers/shop");
 
