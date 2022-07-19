279c112f33705a9e2314d434988d8d782d237996
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-07-18 22:59:21 -0400

    Set up email notification for successful signup

--- server-express-demo-2/controllers/auth.js
+++ server-express-demo-2/controllers/auth.js
@@ -1,6 +1,18 @@
 const bcrypt = require("bcryptjs");
+const nodemailer = require("nodemailer");
+const sendgridTransport = require("nodemailer-sendgrid-transport");
+
+require("dotenv").config();
 
 const User = require("../models/user");
+
+const transporter = nodemailer.createTransport(
+    sendgridTransport({
+        auth: {
+            api_key: process.env.SENDGRID_API_KEY,
+        },
+    })
+);
 
 exports.getLogin = (req, res, next) => {
     let isLoggedIn = "false";
@@ -97,6 +109,15 @@ exports.postSignup = (req, res, next) => {
                 })
                 .then((result) => {
                     res.redirect("/");
+                    return transporter.sendMail({
+                        to: email,
+                        from: "shop@anton-site.com",
+                        subject: "Signup succeeded!",
+                        html: "<h1>You successfully signed up!</h1>",
+                    });
+                })
+                .catch((err) => {
+                    console.log(err);
                 });
         })
         .catch((err) => console.log(err));
