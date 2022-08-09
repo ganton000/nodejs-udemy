a12d5390e8ced735df8cab5b711d559773b132d2
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-08-09 19:22:05 -0400

    Added postReset for resetting password via email and resetToken in auth controller

--- server-express-demo-2/controllers/auth.js
+++ server-express-demo-2/controllers/auth.js
@@ -1,3 +1,5 @@
+const crypto = require("crypto");
+
 const bcrypt = require("bcryptjs");
 const nodemailer = require("nodemailer");
 const sendgridTransport = require("nodemailer-sendgrid-transport");
@@ -142,5 +144,42 @@ exports.getReset = (req, res, next) => {
         path: "/reset",
         docTitle: "Reset Password",
         errorMessage: message,
+    });
+};
+
+exports.postReset = (req, res, next) => {
+    crypto.randomBytes(32, (err, buffer) => {
+        if (err) {
+            console.log(err);
+            return res.redirect("/reset");
+        }
+
+        const token = buffer.toString("hex");
+        User.findOne({ email: req.body.email })
+            .then((user) => {
+                if (!user) {
+                    req.flash("error", "No account with that email found.");
+                    return res.redirect("/reset");
+                }
+                //stores token into Database
+                user.resetToken = token;
+                user.resetTokenExpiration = Date.now() + 3600000; //1 hr after now
+                return user.save();
+            })
+            .then((result) => {
+                res.redirect("/");
+                return transporter.sendMail({
+                    to: req.body.email,
+                    from: process.env.SENDGRID_VERIFIED_SENDER,
+                    subject: "Password reset",
+                    html: `
+                        <p>You requested a password reset</p>
+                        <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>
+                    `,
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     });
 };
