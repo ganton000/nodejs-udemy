860536903f9c14887f55a46b40adcafc8f8c0391
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-08-12 17:39:25 -0400

    Added password validation to signup route

--- server-express-demo-2/routes/auth.js
+++ server-express-demo-2/routes/auth.js
@@ -1,9 +1,31 @@
 const express = require("express");
-const { check } = require("express-validator");
+const { check, body } = require("express-validator");
 
 const authController = require("../controllers/auth");
 
 const router = express.Router();
+
+const signUpValidation = () => {
+    return [
+        check("email")
+            .isEmail()
+            .withMessage("Please enter a valid email")
+            .custom((value, { req }) => {
+                if (value === "test@test.com") {
+                    throw new Error("This email address is forbidden.");
+                }
+                return true;
+            }),
+        body("password")
+            .matches(
+                /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/,
+                "i"
+            )
+            .withMessage(
+                "Password should be combination of one uppercase , one lower case, one special char, one digit and min 8, max 20 char long"
+        )
+    ];
+};
 
 router.get("/login", authController.getLogin);
 
@@ -11,7 +33,7 @@ router.get("/signup", authController.getSignup);
 
 router.post("/login", authController.postLogin);
 
-router.post("/signup", check('email').isEmail().withMessage('Please enter a valid email'), authController.postSignup);
+router.post("/signup", signUpValidation(), authController.postSignup);
 
 router.post("/logout", authController.postLogout);
 
