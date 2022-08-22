26fe93dd72f04e49f7891a30a36e5f9e26f0bca3
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-08-21 21:21:32 -0400

    refactor deleteProduct in delete method

--- server-express-demo-2/controllers/admin.js
+++ server-express-demo-2/controllers/admin.js
@@ -203,8 +203,8 @@ exports.getProducts = (req, res, next) => {
         });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-    const { productId } = req.body;
+exports.deleteProduct = (req, res, next) => {
+    const { productId } = req.params;
 
     Product.findById(productId)
         .then((product) => {
@@ -219,11 +219,13 @@ exports.postDeleteProduct = (req, res, next) => {
         })
         .then(() => {
             console.log("Product Removed");
-            res.redirect("/admin/products");
+            res.status(200).json({
+                message: "Success!",
+            });
         })
         .catch((err) => {
-            const error = new Error(err);
-            error.httpStatusCode = 500;
-            return next(error);
+            res.status(500).json({
+                message: "Deleting product failed.",
+            });
         });
 };
