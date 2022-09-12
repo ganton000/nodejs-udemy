396fbf40ff7cccf4a631d2ec6bb6032ad10354b4
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-09-11 21:02:21 -0400

    set headers for CORS access

--- rest-api-demo/app.js
+++ rest-api-demo/app.js
@@ -6,10 +6,21 @@ const feedRoutes = require("./routes/feed");
 const PORT = 8080;
 const app = express();
 
-
-
 //app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); //application/json
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, PATCH, DELETE"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+    );
+    next();
+});
 
 app.use("/feed", feedRoutes);
 
