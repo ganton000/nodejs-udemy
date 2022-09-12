8d848eac07d60d9f181c2e61bb4a839b78bc0886
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-09-11 20:54:21 -0400

    Created POST route

--- rest-api-demo/app.js
+++ rest-api-demo/app.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const feedRoutes = require("./routes/feed");
 
-const app = express();
 const PORT = 8080;
+const app = express();
+
+
+
+//app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); //application/json
 
 app.use("/feed", feedRoutes);
 
--- rest-api-demo/controllers/feed.js
+++ rest-api-demo/controllers/feed.js
@@ -1,3 +1,25 @@
+//REST Apis don't render html so no res.render()
+
 exports.getPosts = (req, res, next) => {
-	console.log(req);
+    res.status(200).json({
+        posts: [
+            {
+                title: "First Post",
+                content: "This is the first post!",
+            },
+        ],
+    });
+};
+
+exports.createPost = (req, res, next) => {
+    const { title, content } = req.body;
+
+    res.status(201).json({
+        message: "Post created successfully",
+        post: {
+            id: new Date().toISOString(),
+            title: title,
+            content: content,
+        },
+    });
 };
