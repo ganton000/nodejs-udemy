38a3e6579cf5e49a8b09bd31d6cbe05b97323542
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-10-02 21:21:06 -0400

    Added error handling middleware

--- react-REST-api/backend/app.js
+++ react-REST-api/backend/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -10,6 +11,7 @@ const PORT = 8080;
 const app = express();
 
 app.use(bodyParser.json()); //application/json
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -25,6 +27,16 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRoutes);
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+
+    res.status(status).json({
+        message: message
+    });
+});
 
 mongoose
     .connect(process.env.MONGODB_URI)
--- react-REST-api/backend/controllers/feed.js
+++ react-REST-api/backend/controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require("express-validator");
 
+const Post = require("../models/post");
+
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
         posts: [
@@ -21,23 +23,32 @@ exports.createPost = (req, res, next) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-        return res.status(422).json({
-            message: "Validation failed, entered data is incorrect",
-            errors: errors.array(),
-        });
+        const error = new Error("Validation failed, entered data is incorrect");
+        error.statusCode = 422;
+        throw error;
     }
-    const { title, content } = req.body;
 
-    res.status(201).json({
-        message: "Post created successfully!",
-        post: {
-            _id: new Date().toISOString(),
-            title: title,
-            content: content,
-            creator: {
-                name: "Anton",
-            },
-            createdAt: new Date(),
+    const { title, content } = req.body;
+    const post = new Post({
+        title: title,
+        content: content,
+        imageUrl: "images/duck.jpg",
+        creator: {
+            name: "Anton",
         },
     });
+    post.save()
+        .then((result) => {
+            console.log(result);
+            res.status(201).json({
+                message: "Post created successfully!",
+                post: result,
+            });
+        })
+        .catch((err) => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
 };
