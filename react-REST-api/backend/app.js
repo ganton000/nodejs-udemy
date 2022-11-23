2f409db41c48edab18792c094d2ab0b96797f76c
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-11-22 21:55:44 -0500

    Added signup controller and route, including validation middleware to route

--- react-REST-api/backend/app.js
+++ react-REST-api/backend/app.js
@@ -62,9 +62,11 @@ app.use((error, req, res, next) => {
     console.log(error);
     const status = error.statusCode || 500;
     const message = error.message;
+    const data = error.data;
 
     res.status(status).json({
-        message: message,
+        message,
+        data,
     });
 });
 
