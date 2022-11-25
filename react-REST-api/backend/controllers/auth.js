a77a8441b29c85af595265b72868015bdecb0435
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-11-25 17:26:48 -0500

    Added bcrypt to decrypt password for our new User models in mongoose

--- react-REST-api/backend/controllers/auth.js
+++ react-REST-api/backend/controllers/auth.js
@@ -1,4 +1,5 @@
 const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 const User = require("../models/user");
 
@@ -12,4 +13,27 @@ exports.signUp = (req, res, next) => {
     }
 
     const { email, name, password } = req.body;
+
+    bcrypt
+        .hash(password, 12)
+        .then((hashedPassword) => {
+            const user = new User({
+                email,
+                name,
+                password: hashedPassword,
+            });
+            return user.save();
+        })
+        .then((result) => {
+            res.status(201).json({
+                message: "User created!",
+                userId: result._id,
+            });
+        })
+        .catch((err) => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
 };
