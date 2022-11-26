f7a68604748d8464f552573d7dd9672e1d467ec7
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-11-25 21:05:43 -0500

    Updated createPost in feed controller to store userId

--- react-REST-api/backend/controllers/feed.js
+++ react-REST-api/backend/controllers/feed.js
@@ -4,6 +4,7 @@ const path = require("path");
 const { validationResult } = require("express-validator");
 
 const Post = require("../models/post");
+const User = require("../models/user");
 
 const handle500error = (err) => {
     if (!err.statusCode) {
@@ -58,20 +59,31 @@ exports.createPost = (req, res, next) => {
 
     const imageUrl = req.file.path;
     const { title, content } = req.body;
+    let creator;
+
     const post = new Post({
         title: title,
         content: content,
         imageUrl: imageUrl,
-        creator: {
-            name: "Anton",
-        },
+        creator: req.userId,
     });
     post.save()
         .then((result) => {
-            console.log(result);
+            return User.findById(req.userId);
+        })
+        .then((user) => {
+            creator = user;
+            user.posts.push(post);
+            return user.save();
+        })
+        .then((result) => {
             res.status(201).json({
                 message: "Post created successfully!",
                 post: result,
+                creator: {
+                    _id: creator._id,
+                    name: creator.name,
+                },
             });
         })
         .catch((err) => {
