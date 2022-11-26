f82b4f565f6f6c17173470514c5509aa2ce54c17
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-11-26 14:53:30 -0500

    Updated getPosts to async/await syntax

--- react-REST-api/backend/controllers/feed.js
+++ react-REST-api/backend/controllers/feed.js
@@ -13,33 +13,28 @@ const handle500error = (err) => {
     next(err);
 };
 
-exports.getPosts = (req, res, next) => {
+exports.getPosts = async (req, res, next) => {
     const currentPage = req.query.page || 1;
     const perPage = 2;
-    let totalItems;
 
-    Post.find()
-        .countDocuments()
-        .then((count) => {
-            totalItems = count;
+    try {
+        const totalItems = await Post.find().countDocuments();
 
-            return Post.find()
-                .skip((currentPage - 1) * perPage)
-                .limit(perPage);
-        })
-        .then((posts) => {
-            res.status(200).json({
-                message: "Fetched posts successfully.",
-                posts: posts,
-                totalItems,
-            });
-        })
-        .catch((err) => {
-            if (!err.statusCode) {
-                err.statusCode = 500;
-            }
-            next(err);
+        const posts = await Post.find()
+            .skip((currentPage - 1) * perPage)
+            .limit(perPage);
+
+        res.status(200).json({
+            message: "Fetched posts successfully.",
+            posts: posts,
+            totalItems,
         });
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
 };
 
 exports.createPost = (req, res, next) => {
