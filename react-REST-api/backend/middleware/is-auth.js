e53441246a283dccb2b4e8a4c1c9f9f2cd774295
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-11-25 20:58:53 -0500

    Created middleware to parse web token from get requests to authenticate users

---
+++ react-REST-api/backend/middleware/is-auth.js
@@ -0,0 +1,33 @@
+const jwt = require("jsonwebtoken");
+const {
+    default: errorHandler,
+} = require("../../frontend/src/components/ErrorHandler/ErrorHandler");
+
+module.exports = (req, res, next) => {
+    //middleware to be used in routes to
+    //parse token from headers
+
+    const authHeader = req.get("Authorization");
+    if (!authHeader) {
+        const error = new errorHandler("Not authenticated.");
+        error.statusCode = 401;
+        throw error;
+    }
+
+    const token = authHeader.split(" ")[1];
+    try {
+        decodedToken = jwt.verify(token, "supersecret");
+    } catch (err) {
+        err.statusCode = 500;
+        throw err;
+    }
+
+    if (!decodedToken) {
+        const error = new Error("Not authenticated.");
+        error.statusCode = 401;
+        throw error;
+    }
+
+    req.userId = decodedToken.userId;
+    next();
+};
