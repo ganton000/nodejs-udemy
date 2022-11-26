3c8b410eb412a5dda09d6a715eaf73682ca43f61
ganton000 <SendittoGeorgeAnton@gmail.com>
2022-11-25 21:08:43 -0500

    Updated editPost and deletePost to check if userId matches to perform actions

--- react-REST-api/backend/controllers/feed.js
+++ react-REST-api/backend/controllers/feed.js
@@ -141,6 +141,12 @@ exports.updatePost = (req, res, next) => {
                 throw error;
             }
 
+            if (post.creator.toString() !== req.userId) {
+                const error = new Error("Not authorized!");
+                error.statusCode = 403;
+                throw error;
+            }
+
             if (imageUrl !== post.imageUrl) {
                 clearImage(post.imageUrl);
             }
@@ -172,6 +178,12 @@ exports.deletePost = (req, res, next) => {
             if (!post) {
                 const error = new Error("Could not find post.");
                 error.statusCode = 404;
+                throw error;
+            }
+
+            if (post.creator.toString() !== req.userId) {
+                const error = new Error("Not authorized!");
+                error.statusCode = 403;
                 throw error;
             }
 
